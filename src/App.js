43740d1492bd1e52bd91a2d9a29eeac47d320d25
Andi Azamuddin <andiazamuddin@gmail.com>
2024-10-22 23:15:15 +0800

    Initial commit with console log Two Sum

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import { useEffect } from 'react';
 
 function App() {
+  function twoSum(numbers, target) {
+    let left = 0;
+    let right = numbers.length - 1;
+  
+    while (left < right) {
+      const sum = numbers[left] + numbers[right];
+  
+      if (sum === target) {
+        return [left + 1, right + 1];
+      } else if (sum < target) {
+        left++;
+      } else {
+        right--;
+      }
+    }
+
+    return [];
+  }
+  
+  useEffect(() => {
+    console.log('twoSum([4, 11, 17, 25], 21) is: ', twoSum([4, 11, 17, 25], 21));
+    console.log('twoSum([0, 1, 2, 2, 3, 5], 4) is: ', twoSum([0, 1, 2, 2, 3, 5], 4));
+    console.log('twoSum([-1, 0], -1) is: ', twoSum([-1, 0], -1));
+  }, [])
+  
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Inspect the Console to view the answer for Two Sum II - Input Array Is Sorted
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
